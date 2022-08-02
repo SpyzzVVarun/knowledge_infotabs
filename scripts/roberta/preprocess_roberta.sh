fc3e86f6361f516a45141a38db00616b5cb128a3
Varun Nagpal <95134445+SpyzzVVarun@users.noreply.github.com>
2022-08-02 14:06:41 +0530

    Update preprocess_roberta.sh

--- scripts/roberta/preprocess_roberta.sh
+++ scripts/roberta/preprocess_roberta.sh
@@ -2,6 +2,10 @@
 python3 /content/knowledge_infotabs/scripts/roberta/preprocess_roberta.py --max_len 512 --data_dir /content/knowledge_infotabs/temp/ --in_dir data/bpr --out_dir processed/bpr --single_sentence 0
 # drr
 python3 /content/knowledge_infotabs/scripts/roberta/preprocess_roberta.py --max_len 512 --data_dir /content/knowledge_infotabs/temp/ --in_dir data/drr --out_dir processed/drr --single_sentence 0
+# drr_num
+python3 /content/knowledge_infotabs/scripts/roberta/preprocess_roberta.py --max_len 512 --data_dir /content/knowledge_infotabs/temp/ --in_dir data/drr_num --out_dir processed/drr_num --single_sentence 0
+# drr_neg
+python3 /content/knowledge_infotabs/scripts/roberta/preprocess_roberta.py --max_len 512 --data_dir /content/knowledge_infotabs/temp/ --in_dir data/drr_neg --out_dir processed/drr_neg --single_sentence 0
 # kg_explicit
 python3 /content/knowledge_infotabs/scripts/roberta/preprocess_roberta.py --max_len 512 --data_dir /content/knowledge_infotabs/temp/ --in_dir data/kg_explicit --out_dir processed/kg_explicit --single_sentence 0
 #opr
